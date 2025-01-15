af79740ff4c4a1054c6f7e51428f16ece5878524
Ritik11131 <ritik11131@gmail.com>
2025-01-16 03:01:11 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ToastModule } from 'primeng/toast';
+import { Ripple } from 'primeng/ripple';
+import { MessageService } from 'primeng/api';
+import { SelectModule } from 'primeng/select';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { DividerModule } from 'primeng/divider';
+import { AvatarModule } from 'primeng/avatar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule,InputTextModule,CardModule,ToastModule,DividerModule,SelectModule,MultiSelectModule,AvatarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'demo-app';
+  selectedCity:any;
+  cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+];
+  constructor(private messageService: MessageService) {}
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+}
+
+showInfo() {
+    this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content' });
+}
+
+showWarn() {
+    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: 'Message Content' });
+}
+
+showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Message Content' });
+}
+
+showContrast() {
+    this.messageService.add({ severity: 'contrast', summary: 'Error', detail: 'Message Content' });
+}
+
+showSecondary() {
+    this.messageService.add({ severity: 'secondary', summary: 'Secondary', detail: 'Message Content' });
+}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import Noir from '@/app/themes/mytheme'
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: Noir, ripple: false, inputStyle: 'outlined' })
+  ]
 };
