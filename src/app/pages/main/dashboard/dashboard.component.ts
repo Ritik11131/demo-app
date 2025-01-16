c1b21a035b7c3fcee6365196118e4c721da18ab0
Ritik11131 <ritik11131@gmail.com>
2025-01-17 02:55:44 +0530

    Dashboard crds

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ButtonModule],
+  imports: [ButtonModule,AvatarModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
